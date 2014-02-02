faec66842db4a5606705127185ea83230be06d75
Maricris Nonato <maricris.nonato@gmail.com>
2014-02-02 17:56:00 +0800

    Give option to use an image both as a cover-image and as a post image
     - an alt of 'cover-image' combined with another text will use the said image as cover and will also be embedded in the post body
     - use of 'cover-image' alone as alt will just use this as cover image of your post

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,13 +17,17 @@ $(document).ready(function(){
 	})
 	
     /* Since Ghost does not support a post featured image yet, make an specific image the featured one. Thanks to Thomas Cullen (@ThomasCullen92) for this workaround. */ 
-	mainImage = $('img[alt="cover-image"]');
+    mainImage = $('img[alt*="cover-image"]');
+
     if ( mainImage.length > 0){
         mainImageSource = mainImage.attr('src');
-        $('#main-sidebar').css('background', 'url('+mainImageSource+')');
-        mainImage.remove();
+        $('#main-sidebar').css('background', 'url('+mainImageSource+')');        
     }
-	
+
+    /* If you only use 'cover-image' as your alt text, then it is used only as a cover image and will not appear in your post.  Otherwise, combine 'cover-image' with another alt text and it will appear in your post body. */
+    if (mainImage.attr('alt')=='cover-image'){
+        mainImage.remove();
+    }	
 });
 
 $(window).load(function(){
